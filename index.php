b485d629c2e0ae90122eaf2b0d76855c5c6c0326
Marcin Kalinowski <kalinowskixmarcin@o2.pl>
2024-05-27 10:42:34 +0200

    init

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<title>Studio Citrus - Strona w trakcje realizacji
+</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<meta name="Author" content="studiocitrus.pl">
+
+
+
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600' rel='stylesheet' type='text/css'>
+<link href='reset.css' rel='stylesheet' type='text/css'>
+<link href='main.css' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+<main class="child">
+  <header class="logo"><a href="http://studiocitrus.pl/"></a></header>
+  <section class="txt">
+    <article>
+      Strona w trakcje realizacji.
+    </article>
+  </section>
+</main>
+<div class="fb-like" data-href="https://www.facebook.com/Studio-Citrus-875994752435789/" data-layout="standard" data-action="like" data-show-faces="false" data-share="false"></div>
+</body>
+</html>
